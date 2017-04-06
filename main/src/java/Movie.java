fa104c909a046ed02bf45946cca7fae4cd02a19a
alphafoobar <james@ergo.kiwi>
2017-04-06 14:11:20 +0100

    Moved calculation logic to Movie

--- main/src/java/Movie.java
+++ main/src/java/Movie.java
@@ -22,4 +22,37 @@ public class Movie {
         return title;
     }
 
+    int calculatePoints(int daysRented) {
+        int points = 1;
+
+        if (getPriceCode() == NEW_RELEASE && daysRented > 1) {
+            points++;
+        }
+        return points;
+    }
+
+    double calculateAmount(int daysRented) {
+        double thisAmount = 0;
+
+        // determines the amount for each line
+        switch (getPriceCode()) {
+            case REGULAR:
+                thisAmount += 2;
+                if (daysRented > 2) {
+                    thisAmount += (daysRented - 2) * 1.5;
+                }
+                break;
+            case NEW_RELEASE:
+                thisAmount += daysRented * 3;
+                break;
+            case CHILDRENS:
+                thisAmount += 1.5;
+                if (daysRented > 3) {
+                    thisAmount += (daysRented - 3) * 1.5;
+                }
+                break;
+        }
+
+        return thisAmount;
+    }
 }
